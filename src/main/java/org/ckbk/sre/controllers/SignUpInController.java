8c096897ed2a03bf7d15953a848f15aa7e9405af
Denisa <denisa.veres@student.upt.ro>
2022-05-04 22:53:12 +0300

    CKBK-2 GUI for Sign in combined with Sign up

--- src/main/java/org/ckbk/sre/controllers/SignUpInController.java
+++ src/main/java/org/ckbk/sre/controllers/SignUpInController.java
@@ -5,6 +5,7 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.text.Text;
 import org.ckbk.sre.exceptions.EmptyInputFieldException;
+import org.ckbk.sre.exceptions.InvalidCredentialsException;
 import org.ckbk.sre.exceptions.UsernameAlreadyExistsException;
 import org.ckbk.sre.model.User;
 import org.ckbk.sre.services.UserService;
@@ -25,6 +26,12 @@ public class SignUpInController {
     private TextField lastNameField;
     @FXML
     private TextField firstNameField;
+    @FXML
+    private Text logInMessage;
+    @FXML
+    private PasswordField passwordLogInField;
+    @FXML
+    private TextField usernameLogInField;
 
     @FXML
     public void handleRegisterAction() {
@@ -35,6 +42,18 @@ public class SignUpInController {
             registrationMessage.setText(e.getMessage());
         } catch (UsernameAlreadyExistsException e) {
             registrationMessage.setText(e.getMessage());
+        }
+    }
+
+    @FXML
+    public void handleLogInAction() {
+        try {
+            UserService.logInUser(usernameLogInField.getText(), passwordLogInField.getText());
+            logInMessage.setText("Successful log in!");
+        } catch (EmptyInputFieldException e) {
+            logInMessage.setText(e.getMessage());
+        } catch (InvalidCredentialsException e) {
+            logInMessage.setText(e.getMessage());
         }
     }
 }
