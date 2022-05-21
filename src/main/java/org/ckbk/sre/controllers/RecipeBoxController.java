fa207b7b8a30d13454a24277835edaffbe4ae4ab
Denisa <denisa.veres@student.upt.ro>
2022-05-22 00:01:54 +0300

    CKBK-15 Retrieved data from file

--- src/main/java/org/ckbk/sre/controllers/RecipeBoxController.java
+++ src/main/java/org/ckbk/sre/controllers/RecipeBoxController.java
@@ -14,7 +14,7 @@ import org.ckbk.sre.services.RecipeService;
 
 public class RecipeBoxController {
     @FXML
-    public Text type;
+    private Text type;
     @FXML
     private Text title;
     @FXML
--- src/main/java/org/ckbk/sre/controllers/SettingsController.java
+++ src/main/java/org/ckbk/sre/controllers/SettingsController.java
@@ -1,6 +1,52 @@
 package org.ckbk.sre.controllers;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
+import org.ckbk.sre.services.UserService;
+
 public class SettingsController {
+    @FXML
+    private Text roleField;
+    @FXML
+    private Text lastNamePrevField;
+    @FXML
+    private TextField lastNameField;
+    @FXML
+    private Text firstNamePrevField;
+    @FXML
+    private TextField firstNameField;
+    @FXML
+    private Text mailPrevField;
+    @FXML
+    private TextField mailField;
+    @FXML
+    private Text nrTelPrevField;
+    @FXML
+    private TextField nrTelField;
+    @FXML
+    private Text usernameField;
+    @FXML
+    private PasswordField passwordFieldEdit;
+    @FXML
+    private PasswordField passwordFieldEditConfirm;
+    @FXML
+    private PasswordField passwordFieldConfirm;
+    @FXML
+    private Text confirmationMessage;
+
+    @FXML
+    public void initialize(){
+        usernameField.setText(UserService.getUser().getUsername());
+        roleField.setText(String.valueOf(UserService.getUser().getRole()));
+        firstNamePrevField.setText(UserService.getUser().getFirstName());
+        lastNamePrevField.setText(UserService.getUser().getLastName());
+        nrTelPrevField.setText(UserService.getUser().getNrTel());
+        mailPrevField.setText(UserService.getUser().getMail());
+    }
+
+    @FXML
     public void handleEditAction() {
     }
 }
